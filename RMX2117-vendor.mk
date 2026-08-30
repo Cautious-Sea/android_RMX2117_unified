2ba164f868f0fe6ff1090791a726cf2827d0a45a
Cautious-Sea <user.cautiousxsea@gmail.com>
2026-08-30 21:08:29 +0530

    RMX2117: Remove duplicate mediatek-common from PRODUCT_PACKAGES
    The build failed with multiple rules generate boot-mediatek-common.oat.
    This was because mediatek-common was added multiple times to PRODUCT_PACKAGES and PRODUCT_BOOT_JARS.
    Removed redundant mediatek-common since it is already included via mediatek-frameworks.mk.
    
    Change-Id: Ied414e3b5e955eec3a57233bfe818eeac2fdef70

--- RMX2117-vendor.mk
+++ RMX2117-vendor.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2117/proprietary/system_ext/lib64/vendor.mediatek.hardware.videotelephony@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.mediatek.hardware.videotelephony@1.0.so
 
 PRODUCT_PACKAGES += \
-    mediatek-common \
     mediatek-framework \
     mediatek-gwsd \
     mediatek-gwsdv2 \
