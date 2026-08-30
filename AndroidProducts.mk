f3cb236fc3874e2322696b6ad9d8b2db23458854
Cautious-Sea <user.cautiousxsea@gmail.com>
2026-08-30 19:14:13 +0530

    Update COMMON_LUNCH_CHOICES format
    Change-Id: Iea0601dca7083a23e898c14e0a903eac2d209bcc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,7 +18,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_RMX2117.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX2117-user \
+    lineage_RMX2117-bp4a-user \
     lineage_RMX2117-bp4a-userdebug \
-    lineage_RMX2117-userdebug \
-    lineage_RMX2117-eng
+    lineage_RMX2117-bp4a-eng
